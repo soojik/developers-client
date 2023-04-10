81430a3b87b5460b6dede07e3b91b5db6610522b
JeongYeop <117172805+ITfervor@users.noreply.github.com>
2023-04-10 14:56:28 +0900

    Backup changes to App.tsx and Nav.tsx files

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,91 +1,106 @@
+import { Link, useNavigate } from "react-router-dom";
+import NotificationIcon from "./icons/NotificationIcon";
+import PersonIcon from "./icons/PersonIcon";
+import MenuBarIcon from "./icons/MenuBarIcon";
+import { useState } from "react";
+import MenuCloseIcon from "./icons/MenuCloseIcon";
+
 const Nav = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const navigate = useNavigate();
+  const isLoggedIn = false;
+
+  const handleOpenMenuBar = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
   return (
-    <nav className="bg-gray-800 px-4 py-3">
+    <nav className="px-4 py-3 bg-gray-800">
       <div className="flex items-center justify-between">
-        <div className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img
-            className="h-8 w-auto"
+            className="w-auto h-8"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-            alt="Workflow"
+            alt="로고 이미지"
           />
-          <div className="ml-4 text-white font-bold">Developers</div>
-        </div>
+          <div className="ml-4 font-bold text-white">Developers</div>
+        </Link>
         <div className="hidden md:block">
           <div className="flex items-center">
-            <a
-              href="#"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-gray-300 hover:bg-gray-700"
+            <Link
+              to="/problem"
+              className="px-3 py-2 text-sm font-medium text-white rounded-md hover:text-gray-300 hover:bg-gray-700"
             >
               문제 풀이
-            </a>
-            <a
-              href="#"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-gray-300 hover:bg-gray-700 ml-4"
+            </Link>
+            <Link
+              to="/mentoring"
+              className="px-3 py-2 ml-4 text-sm font-medium text-white rounded-md hover:text-gray-300 hover:bg-gray-700"
             >
-              화상 채팅
-            </a>
-            <a
-              href="#"
-              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-gray-300 hover:bg-gray-700 ml-4"
-            >
-              커뮤니티
-            </a>
+              멘토링
+            </Link>
           </div>
         </div>
 
-        <div className="ml-4 flex-shrink-0 flex">
-          <button className="bg-gray-700 text-white rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M20 17.7V9a7 7 0 00-14 0v8.7l-2 2v.3h18v-.3l-2-2z"
-              />
-            </svg>
+        <div className="flex items-center">
+          <button className="p-2 mr-1">
+            <NotificationIcon fill="white" />
           </button>
-          {/* 모바일 화면에서만 햄버거 메뉴 표시 */}
-          <div className="md:hidden">
-            <button className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">Open user menu</span>
-              <svg
-                className="h-6 w-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
           <div className="hidden md:block">
-            <button className="bg-gray-700 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full"
-                src="https://avatars.githubusercontent.com/u/4357284?v=4"
-                alt="Profile"
-              />
+            {isLoggedIn ? (
+              <button>로그인한 유저이미지</button>
+            ) : (
+              <button className="p-2" onClick={() => navigate("/login")}>
+                <PersonIcon fill="white" />
+              </button>
+            )}
+          </div>
+          {/* 모바일 화면에서 햄버거 메뉴 표시 */}
+          <div className="md:hidden">
+            <button className={`p-2 ${isOpenMenu ? "hidden" : "block"}`}>
+              <MenuBarIcon fill="white" />
             </button>
+
+            {/* <div
+              className={`${
+                isOpenMenu
+                  ? "absolute left-0 w-full h-screen bg-neutral-700"
+                  : ""
+              }`}
+            >
+              <div
+                className={`bg-white fixed top-0 right-0 h-full w-48 z-10 ${
+                  isOpenMenu ? "" : "translate-x-0"
+                }`}
+                onBlur={() => setIsOpenMenu(false)}
+              >
+                <button
+                  className={`absolute top-0 p-1 -left-20 translate-x-full bg-transparent border-none ${
+                    isOpenMenu ? "hidden" : "block"
+                  }`}
+                  onClick={handleOpenMenuBar}
+                >
+                  <MenuCloseIcon fill="white" />
+                </button>
+                <div
+                  className="text-xl hover:font-bold"
+                  onClick={() => setIsOpenMenu(false)}
+                >
+                  <Link to="/">홈</Link>
+                </div>
+                <div
+                  className="text-xl hover:font-bold"
+                  onClick={() => setIsOpenMenu(false)}
+                >
+                  <Link to="/problem">문제 풀이</Link>
+                </div>
+                <div
+                  className="text-xl hover:font-bold"
+                  onClick={() => setIsOpenMenu(false)}
+                >
+                  <Link to="/mentoring">멘토링</Link>
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>

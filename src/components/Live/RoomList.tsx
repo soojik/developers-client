ee5ea15ff425a6988457c0fab9827225ce0b1357
김지수 <wltn2858@swu.ac.kr>
2023-04-11 18:37:58 +0900

    feat: 방 이미지 및 소개 클릭하면 방 정보 모달 창 띄우기

--- src/components/Live/RoomList.tsx
+++ src/components/Live/RoomList.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ShowMentoring from './ShowMentoring';
 
 interface Room {
     id: number;
@@ -13,17 +14,23 @@ interface RoomListProps {
 }
 
 const RoomList: React.FC<RoomListProps> = ({ rooms }) => {
+    const [showRoomInfo, setShowRoomInfo] = useState(false);
+
+    const handleOpenRoomInfo = () => {
+        setShowRoomInfo(true);
+    }
 
     return (
         <div className="flex flex-wrap justify-center">
             {rooms.map((room) => (
                 <div key={room.id} className="w-1/3 p-4 flex flex-col justify-center items-center">
-                    <img className="h-50" src={room.thumbnailUrl} alt={room.title} />
-                    <h2 className="text-lg font-semibold mt-2">{room.title}</h2>
-                    <h3 className=''>{room.description}</h3>
+                    <img className="h-50" src={room.thumbnailUrl} alt={room.title} onClick={handleOpenRoomInfo} />
+                    <h2 className="text-lg font-semibold mt-2" onClick={handleOpenRoomInfo} >{room.title}</h2>
+                    <h3 className='' onClick={handleOpenRoomInfo} >{room.description}</h3>
                     <h3 className=''>{room.mentorName}</h3>
                 </div>
             ))}
+            { showRoomInfo && <ShowMentoring handleClose={() => setShowRoomInfo(false)} /> }
         </div>
     );
 }

ecb3e775ecd9b28880f03ea65c5cefc6c74d2a89
moon <xmun74@naver.com>
2023-04-11 10:10:24 +0900

    feat: login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,105 @@
+import axios from "axios";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
+interface LoginProps {
+  memberId: string;
+  password: string;
+}
+
 const Login = () => {
-  return <>로그인</>;
+  const URL = "http://localhost";
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors,
+  } = useForm<LoginProps>({ mode: "onChange" });
+
+  const handleLoginSubmit: SubmitHandler<LoginProps> = (data) => {
+    console.log("보낸값", data);
+    axios
+      .post(`${URL}/api/auth/local`, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        let accessToken = res.headers.authorization;
+        let refreshToken = res.headers.refresh;
+
+        axios.defaults.headers.common["Authorization"] = `${accessToken}`;
+
+        console.log("로그인 응답", res);
+        // navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <div className="w-4/5">
+        <div className="flex justify-center text-2xl font-light">로그인</div>
+        <form
+          className="flex justify-center mt-10"
+          onSubmit={handleSubmit(handleLoginSubmit)}
+        >
+          <div className="flex flex-col w-full max-w-lg">
+            <label className="mt-4 text-xs">이메일</label>
+            <input
+              type="email"
+              placeholder="example@developers.com"
+              className="sign_input"
+              {...register("memberId", {
+                required: "이메일을 올바르게 입력해주세요.",
+                pattern: {
+                  value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  message: "이메일을 올바르게 입력해주세요.",
+                },
+              })}
+            />
+            <div className="text-xs text-red-500">
+              {errors?.memberId?.message}
+            </div>
+
+            <label className="mt-4 text-xs">비밀번호</label>
+            <input
+              type="password"
+              placeholder="******"
+              className="sign_input"
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                minLength: {
+                  value: 6,
+                  message: "6자 이상 18자 이하의 비밀번호를 입력해주세요.",
+                },
+                maxLength: {
+                  value: 18,
+                  message: "18자 이하의 비밀번호를 입력해주세요",
+                },
+              })}
+            />
+            <div className="text-xs text-red-500">
+              {errors?.password?.message}
+            </div>
+
+            <button type="submit" className="form_submit_btn">
+              로그인
+            </button>
+            <div className="flex justify-between font-light">
+              아직 계정이 없다면?
+              <Link
+                to="/register"
+                className="font-semibold text-accent-400 hover:text-accent-500"
+              >
+                회원가입
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Login;

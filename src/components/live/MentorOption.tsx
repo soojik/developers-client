222d60810bd1ff0dfafccada7059cf67d9db64fb
paduck-96 <peterrabitt1234@naver.com>
2023-04-13 14:12:54 +0900

    feat: delete room

---
+++ src/components/live/MentorOption.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MentorOption = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MentorOption;
--- src/components/live/RoomList.tsx
+++ src/components/live/RoomList.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ShowMentoring from './ShowMentoring';
+import ShowMentorOption from './ShowMentorOption';
 export interface Room {
     mentoringRoomId:Number,
     title:String,
@@ -16,26 +17,34 @@ interface RoomListProps {
 const RoomList: React.FC<RoomListProps> = ({ rooms }) => {
     const [selectedRoom, setSelectedRoom] = useState<Room | null>(null);
     const [showRoomInfo, setShowRoomInfo] = useState(false);
+    const [isMentor, setIsMentor] = useState(false) //멘토 멘티 상태값
+    const [showMentorOption, setShowMentorOption] = useState(false); //멘토 설정
 
     const handleShowRoomInfo = (room: Room) => {
         setSelectedRoom(room);
         setShowRoomInfo(!showRoomInfo);
       };
-      
 
-    let num = 1;
+      const handleShowMentorOption = (room:Room)=>{
+        setSelectedRoom(room);
+        setShowMentorOption(!showMentorOption);
+      }
+      
+    let num = 1; // 출력 갯수 확인 변수
 
     return (
         <div className="flex flex-wrap justify-center" >
-            {rooms.map((room, idx) => (
-                <div key={num++} className="w-1/3 p-4 flex flex-col justify-center items-center">
-                    <h2 className="text-lg font-semibold mt-2" onClick={()=>handleShowRoomInfo(room)}>{idx+1}{room.title}</h2>
+            {rooms.map((room) => (
+                <div key={room.mentoringRoomId.toString()} className="w-1/3 p-4 flex flex-col justify-center items-center">
+                    <h2 className="text-lg font-semibold mt-2" onClick={isMentor?()=>handleShowMentorOption(room):()=>handleShowRoomInfo(room)}>{num++}{room.title}</h2>
                     <h3 className=''>{room.description}</h3>
                     <h3 className=''>{room.mentorName}</h3>
                 </div>
             ))}
             {showRoomInfo && selectedRoom && (
                 <ShowMentoring handleClose={() => setShowRoomInfo(false)} room={selectedRoom} />
+            ) || showMentorOption && selectedRoom && (
+                <ShowMentorOption handleClose={()=> setShowMentorOption(false)} room={selectedRoom} />
             )}
         </div>
     );

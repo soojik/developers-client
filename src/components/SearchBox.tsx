940c32c11a7f36c942699115d307cd22304a3984
moon <xmun74@naver.com>
2023-04-14 12:28:46 +0900

    feat: 문제풀이 레이아웃 정리, SearchBox UI수정, useIntersect hook

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,60 +1,56 @@
+import { useState } from "react";
 import axios from "axios";
-import React from "react";
-import {useState } from "react";
-const Search = () => {
 
-    const [searchTerm, setSearchTerm] = useState("");
+const SearchBox = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-        setSearchTerm(e.target.value)
-    } 
-    
-    const searchProblems = async() =>{
-        try{
-            const response = await axios.get("http://localhost/problem", {
-                params:{
-                    search: searchTerm,
-                },
-            });
-            console.log(response.data)
-        }catch(error){
-            if (axios.isAxiosError(error)) {
-                console.error("Axios error:", error.message, "Code:", error.code);
-              } else {
-                console.error("Unknown error:", error); 
-              }
-        }
-    };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
-    return (
-        <div className="flex h-50vj w-9/10 items-center justify-center p-1">
-            <div className="w-9/10 rounded-lg bg-gray-200 p-4 w-3/4">
-                <div className="flex">
-                    <div className="flex w-8 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5">
-                        <svg viewBox="0 0 20 20" aria-hidden="true" className="pointer-events-none absolute w-5 fill-gray-500 transition">
-                            <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
-                        </svg>
-                    </div>
-                    <input
-            type="text"
-            className="w-full bg-white pl-2 text-base font-semibold outline-0"
-            placeholder=""
-            id=""
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          <input
-            type="button"
-            value="Search"
-            className="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors"
-            onClick={searchProblems}
-          />
-                </div>
-            </div>
-        </div>
-        
-    )
-}
-export default Search;
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("http://localhost/problem", {
+        params: {
+          search: searchTerm,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Axios error:", error.message, "Code:", error.code);
+      } else {
+        console.error("Unknown error:", error);
+      }
+    }
+  };
 
-
+  return (
+    <div className="w-full flex justify-center">
+      <form
+        className="flex rounded-lg sign_input h-[40px] w-full"
+        onSubmit={handleFormSubmit}
+      >
+        <button className="cursor-pointer flex items-center">
+          <svg
+            viewBox="0 0 20 20"
+            aria-hidden="true"
+            className="pointer-events-none absolute w-5 fill-gray-500 transition"
+          >
+            <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
+          </svg>
+        </button>
+        <input
+          type="text"
+          className="w-full pl-8 outline-0 placeholder:text-slate-500 placeholder:font-thin"
+          placeholder="검색어를 입력해주세요"
+          id=""
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+};
+export default SearchBox;
--- src/pages/Problem.tsx
+++ src/pages/Problem.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import ProblemListupdate from "components/problem/ProblemListupdate";
 import SearchBox from "components/SearchBox";
@@ -7,6 +6,7 @@ import DropBoxLevel from "components/dropbox/DropBoxLevel";
 import DropBoxType from "components/dropbox/DropBoxType";
 import DropBoxSolved from "components/dropbox/DropBoxSolved";
 import HashTagComponent from "../components/problem/HashTagComponent";
+import ConfirmBtn from "components/buttons/CofirmBtn";
 
 const ProblemMain = () => {
   const section = [
@@ -67,7 +67,6 @@ const ProblemMain = () => {
       likes: 0,
     },
   ].slice(0, 5);
-  const [isHovered, setIsHovered] = React.useState(false);
 
   const navigate = useNavigate();
 
@@ -75,152 +74,50 @@ const ProblemMain = () => {
     navigate("/problem/register");
   };
 
-  const navigatorToDetail = () =>{
-    navigate("/problem/detail/sb")
-  }
+  const navigatorToDetail = () => {
+    navigate("/problem/detail/sb");
+  };
 
   return (
-    <>
-      {/* 모바일 */}
-      <div className="md:hidden flex flex-col gap-3">
-        <div className="md:hidden flex flex-col gap-3">
-          <DropBoxLevel
-            selectName="Level을 선택하세요"
-            options={["Gold", "Silver", "Bronze"]}
-            paramName="level"
-          />
-          <DropBoxSolved
-            selectName="solved를 선택하세요"
-            options={["Solved", "Solve"]}
-            paramName="solved"
-          />
-          <DropBoxType
-            selectName="Type을 선택하세요"
-            options={["Choice", "Answer"]}
-            paramName="type"
-          />
-        </div>
-
-        <div className=" flex-col md:grid grid-cols-1 gap-1 w-full">
-          <SearchBox />
-          <HashTagComponent />
-          <DropBoxStack
-            selectName="조건을 선택하세요"
-            options={["최신순", "추천순", "조회순"]}
-            paramName=""
-          />
-        </div>
-        <h2
-          className="bg-gray-400 relative group-slate-300 rounded-lg font-bold text-xl text-white text-center"
-          style={{
-            height: "50px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Problem
-        </h2>
-        <ProblemListupdate section={section} sectionHeader={""} />
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className="group rounded-2xl h-10 w-24 bg-blue-500 font-bold text-10 text-white relative overflow-hidden"
-            onClick={navigateToRegister}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            문제 등록
-            <div
-              className={`absolute duration-200 inset-0 w-full h-full transition-all scale-0 ${
-                isHovered ? "scale-100 bg-white/30" : ""
-              } rounded-2xl`}
-            />
-          </button>
-        </div>
+    <div className="md:m-auto w-full md:w-4/5">
+      <div className="flex justify-end mt-5 mb-10">
+        <ConfirmBtn type="submit" onClick={() => navigate("/problem/register")}>
+          문제 등록
+        </ConfirmBtn>
       </div>
-      {/* 데스크탑 */}
-      <div className="hidden md:grid grid-cols-1 gap-4 w-full">
-        <div className="hidden md:grid grid-cols-4 gap-4 w-full  ">
-          <DropBoxLevel
-            selectName="Level을 선택하세요"
-            options={["Gold", "Silver", "Bronze"]}
-            paramName="level"
-          />
-          <DropBoxSolved
-            selectName="solved를 선택하세요"
-            options={["Solved", "Solve"]}
-            paramName="solved"
-          />
-          <DropBoxType
-            selectName="Type을 선택하세요"
-            options={["Choice", "Answer"]}
-            paramName="type"
-          />
-        </div>
-        <div className="hidden flex-col md:grid grid-cols-1 gap-1 w-full">
+      <div className="flex justify-center mb-10">
+        <div className="flex flex-col w-full">
           <SearchBox />
-
-          <HashTagComponent />
-
-          <div style={{ display: "flex", justifyContent: "flex-end" }}>
-            <DropBoxStack
-              selectName="조건을 선택하세요"
-              options={["최신순", "추천순", "조회순"]}
-              paramName=""
+          <div className="flex w-full justify-between">
+            <DropBoxLevel
+              selectName="Level을 선택하세요"
+              options={["Gold", "Silver", "Bronze"]}
+              paramName="level"
+            />
+            <DropBoxSolved
+              selectName="solved를 선택하세요"
+              options={["Solved", "Solve"]}
+              paramName="solved"
+            />
+            <DropBoxType
+              selectName="Type을 선택하세요"
+              options={["Choice", "Answer"]}
+              paramName="type"
             />
           </div>
-        </div>
-        <h2
-          className="bg-gray-400 relative group-slate-300 rounded-lg font-bold text-xl text-white text-center"
-          style={{
-            height: "50px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Problem
-        </h2>
-
-        <ProblemListupdate section={section} sectionHeader={""} />
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className="group rounded-2xl h-10 w-24 bg-blue-500 font-bold text-10 text-white relative overflow-hidden"
-            onClick={navigateToRegister}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            문제 등록
-            <div
-              className={`absolute duration-200 inset-0 w-full h-full transition-all scale-0 ${
-                isHovered ? "scale-100 bg-white/30" : ""
-              } rounded-2xl`}
-            />
-          </button>
-          {/* test */}
-          <button
-            type="button"
-            className="group rounded-2xl h-10 w-24 bg-blue-500 font-bold text-10 text-white relative overflow-hidden"
-            onClick={navigatorToDetail}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            문제 상세페이지 이동
-            <div
-              className={`absolute duration-200 inset-0 w-full h-full transition-all scale-0 ${
-                isHovered ? "scale-100 bg-white/30" : ""
-              } rounded-2xl`}
-            />
-          </button>
-                    {/* test */}
-
-
-          
+          <HashTagComponent />
         </div>
       </div>
-    </>
+      <div className="flex justify-between items-center my-4">
+        <h2 className="text-accent-500 font-bold text-xl">문제 목록</h2>
+        <DropBoxStack
+          selectName="조건을 선택하세요"
+          options={["최신순", "추천순", "조회순"]}
+          paramName=""
+        />
+      </div>
+      <ProblemListupdate section={section} sectionHeader={""} />
+    </div>
   );
 };
 

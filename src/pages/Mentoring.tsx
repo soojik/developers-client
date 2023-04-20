fe4a872bd7ebebb1b7a9f17e48c00eac80da1a64
paduck-96 <peterrabitt1234@naver.com>
2023-04-20 23:21:07 +0900

    fix: mentorig btn css modify && notify

--- src/pages/Mentoring.tsx
+++ src/pages/Mentoring.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import ShowSchedule from '../components/live/ShowSchedule';
-import LiveList from '../components/live/LiveList';
-import MentorScheduling from '../components/live/MentorScheduling';
-import { useRecoilState, useRecoilValue} from 'recoil';
-import { subscriptionState } from '../recoil/subscriptionState';
+import React, { useEffect, useState } from "react";
+import ShowSchedule from "../components/live/ShowSchedule";
+import LiveList from "../components/live/LiveList";
+import MentorScheduling from "../components/live/MentorScheduling";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { subscriptionState } from "../recoil/subscriptionState";
 import { memberInfoState } from "recoil/userState";
 import { axiosInstance } from "apis/axiosConfig";
-
-import axios from 'axios';
 
 export interface ScheduleProps {
   scheduleId: number;
@@ -16,8 +14,8 @@ export interface ScheduleProps {
   menteeName: string;
   startDate: string;
   endDate: string;
-  mentoringRoomId:number; // 방 id로 조회
-};
+  mentoringRoomId: number; // 방 id로 조회
+}
 
 export interface EventProp {
   title: string;
@@ -25,16 +23,19 @@ export interface EventProp {
   endDate: Date;
   type: string;
   mentoringRoomId: number; // 방 id 로 조회
-  scheduleId:number; //일정 삭제 필요
+  scheduleId: number; //일정 삭제 필요
   owner: String; // 방 주인 확인
-};
+}
 
 /**
  * 테스트를 위한 상태값
  */
-const email ="" //테스트 이메일 계정
+const email = ""; //테스트 이메일 계정
 
-const convertScheduleToEvents = (schedules: ScheduleProps[], isMentor:Boolean): EventProp[] => {
+const convertScheduleToEvents = (
+  schedules: ScheduleProps[],
+  isMentor: Boolean
+): EventProp[] => {
   const events: EventProp[] = [];
 
   if (schedules.length) {
@@ -44,25 +45,24 @@ const convertScheduleToEvents = (schedules: ScheduleProps[], isMentor:Boolean): 
           title: `${schedule.mentoringRoomTitle} with ${schedule.mentorName}`,
           startDate: new Date(schedule.startDate),
           endDate: new Date(schedule.endDate),
-          type: 'mentor',
-          mentoringRoomId:schedule.mentoringRoomId,
+          type: "mentor",
+          mentoringRoomId: schedule.mentoringRoomId,
           scheduleId: schedule.scheduleId,
-          owner: schedule.mentorName
+          owner: schedule.mentorName,
         };
         events.push(event);
         console.log(event);
       });
-    }
-    else {
+    } else {
       schedules.forEach((schedule) => {
         const event: EventProp = {
           title: `${schedule.mentoringRoomTitle} with ${schedule.menteeName}`,
           startDate: new Date(schedule.startDate),
           endDate: new Date(schedule.endDate),
-          type: 'mentee',
-          mentoringRoomId:schedule.mentoringRoomId,
+          type: "mentee",
+          mentoringRoomId: schedule.mentoringRoomId,
           scheduleId: schedule.scheduleId,
-          owner: schedule.mentorName
+          owner: schedule.mentorName,
         };
         events.push(event);
       });
@@ -70,28 +70,34 @@ const convertScheduleToEvents = (schedules: ScheduleProps[], isMentor:Boolean): 
   }
 
   return events;
-}
+};
 
 const Mentoring = () => {
-  const { memberInfo, memberId, isLoggedIn } = useRecoilValue(memberInfoState); 
+  const { memberInfo, memberId, isLoggedIn } = useRecoilValue(memberInfoState);
 
-  const [mySchedulesAsMentor, setMySchedulesAsMentor] = useState<ScheduleProps[]>([]);
-  const [mySchedulesAsMentee, setMySchedulesAsMentee] = useState<ScheduleProps[]>([]);
+  const [mySchedulesAsMentor, setMySchedulesAsMentor] = useState<
+    ScheduleProps[]
+  >([]);
+  const [mySchedulesAsMentee, setMySchedulesAsMentee] = useState<
+    ScheduleProps[]
+  >([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const [subscriptions, setSubscriptions] = useRecoilState(subscriptionState);
   const [sse, setSse] = useState<EventSource[] | null>(null); //sse 상태 추적
- 
+
   // 구독 정보 가져오는 함수
   useEffect(() => {
     const fetchSubscriptions = async () => {
       try {
-        if(!subscriptions.length){
-          const response = await axiosInstance.get(`${process.env.REACT_APP_NOTIFY_URL}/subscriptions?userName=${memberInfo.nickname}`);
+        if (!subscriptions.length) {
+          const response = await axiosInstance.get(
+            `${process.env.REACT_APP_NOTIFY_URL}/subscriptions?userName=${memberInfo.nickname}`
+          );
           setSubscriptions(response.data);
         }
       } catch (error) {
-        console.error('구독 정보를 가져오는데 실패했습니다:', error);
+        console.error("구독 정보를 가져오는데 실패했습니다:", error);
       }
     };
     fetchSubscriptions();
@@ -101,19 +107,21 @@ const Mentoring = () => {
   useEffect(() => {
     const eventSources: EventSource[] = [];
 
-    if(!subscriptions.length){
-      subscriptions.forEach((subscription: { mentorName: any; }) => {
-        const es = new EventSource(`${process.env.REACT_APP_DEV_URL}/api/listen?mentorName=${subscription.mentorName}&userName=${memberInfo.nickname}&email=${email}`);
-        es.addEventListener('push', (e) => {
+    if (!subscriptions.length) {
+      subscriptions.forEach((subscription: { mentorName: any }) => {
+        const es = new EventSource(
+          `${process.env.REACT_APP_DEV_URL}/api/listen?mentorName=${subscription.mentorName}&userName=${memberInfo.nickname}&email=${email}`
+        );
+        es.addEventListener("push", (e) => {
           new Notification(e.data);
           console.log(e.data);
         });
-  
+
         eventSources.push(es);
       });
-  
+
       setSse(eventSources);
-  
+
       return () => {
         eventSources.forEach((es) => es.close());
       };
@@ -124,20 +132,20 @@ const Mentoring = () => {
     // API와 통신하여 나의 모든 스케쥴(mySchedule) 가져오고,
     axiosInstance({
       url: `${process.env.REACT_APP_DEV_URL}/api/schedules/mentor/${memberId}`,
-      method: 'get',
+      method: "get",
     }).then((res) => {
-      setMySchedulesAsMentor(res.data['data']);
-      console.log(res.data['data'])
-    })
+      setMySchedulesAsMentor(res.data["data"]);
+      console.log(res.data["data"]);
+    });
     axiosInstance({
       url: `${process.env.REACT_APP_DEV_URL}/api/schedules/mentee/${memberId}`,
-      method: 'get',
+      method: "get",
     }).then((res) => {
       // 멘티 일정 처리
-      setMySchedulesAsMentee(res.data['data']);
-      console.log(res.data['data'])
-    })
-  }, [])
+      setMySchedulesAsMentee(res.data["data"]);
+      console.log(res.data["data"]);
+    });
+  }, []);
 
   const handleClickScheduler = () => {
     setCurrentPage(1);
@@ -145,52 +153,74 @@ const Mentoring = () => {
 
   const handleClickRoomList = () => {
     setCurrentPage(2);
-  }
+  };
 
   const handleClickMentorSetting = () => {
     setCurrentPage(3);
-  }
+  };
 
   return (
     <div className="container mx-auto">
       <div className="flex mt-5 mb-3 justify-center">
         <button
-          className={`flex-1 py-2 px-4 border text-center ${currentPage == 1 ? 'bg-blue-500 border-blue-500' : 'bg-blue-300'
-            }`}
+          className={`flex-1 py-2 px-4 border text-center w-32 md:w-40 lg:w-32 rounded ${
+            currentPage == 1
+              ? "bg-blue-300 text-white border-blue-300"
+              : "border-blue-300 text-blue-300"
+          }`}
           onClick={handleClickScheduler}
         >
           일정 관리
         </button>
         <button
-          className={`flex-1 py-2 px-4 border text-center ${currentPage == 2 ? 'bg-blue-500 border-blue-500' : 'bg-blue-300'
-            }`}
+          className={`flex-1 py-2 px-4 border text-center w-32 md:w-40 lg:w-32 rounded ${
+            currentPage == 2
+              ? "bg-blue-300 text-white border-blue-300"
+              : "border-blue-300 text-blue-300"
+          }`}
           onClick={handleClickRoomList}
         >
           전체 방 목록
         </button>
-        {memberInfo.mentor &&
+        {memberInfo.mentor && (
           <button
-          className={`flex-1 py-2 px-4 border text-center ${currentPage == 3 ? 'bg-blue-500 border-blue-500' : 'bg-blue-300'
+            className={`flex-1 py-2 px-4 border text-center w-32 md:w-40 lg:w-32 rounded ${
+              currentPage == 3
+                ? "bg-blue-300 text-white border-blue-300"
+                : "border-blue-300 text-blue-300"
             }`}
             onClick={handleClickMentorSetting}
           >
             멘토 관리
           </button>
-        }
+        )}
       </div>
       {currentPage == 1 && (
-        <ShowSchedule events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(convertScheduleToEvents(mySchedulesAsMentee, false))} isMentor={memberInfo.mentor} />
+        <ShowSchedule
+          events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(
+            convertScheduleToEvents(mySchedulesAsMentee, false)
+          )}
+          isMentor={memberInfo.mentor}
+        />
       )}
 
       {currentPage == 2 && (
-        <LiveList events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(convertScheduleToEvents(mySchedulesAsMentee, false))}></LiveList>
+        <LiveList
+          events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(
+            convertScheduleToEvents(mySchedulesAsMentee, false)
+          )}
+        ></LiveList>
       )}
 
       {currentPage == 3 && (
-        <MentorScheduling events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(convertScheduleToEvents(mySchedulesAsMentee, false))}></MentorScheduling>
+        <MentorScheduling
+          events={convertScheduleToEvents(mySchedulesAsMentor, true).concat(
+            convertScheduleToEvents(mySchedulesAsMentee, false)
+          )}
+        ></MentorScheduling>
       )}
     </div>
   );
-}
+};
 
 export default Mentoring;

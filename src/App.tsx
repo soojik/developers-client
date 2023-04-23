41380741a595eea07b4d810885ed715bb132e3f0
paduck-96 <peterrabitt1234@naver.com>
2023-04-24 02:53:42 +0900

    fix: condition add

--- src/App.tsx
+++ src/App.tsx
@@ -27,97 +27,103 @@ const App: React.FC = () => {
   const { memberInfo } = useRecoilValue(memberInfoState);
   const [retryCount, setRetryCount] = useState(0);
 
-  useEffect(() => {
-    if (
-      JSON.stringify(memberInfo) !== "{}" &&
-      !Object.keys(memberInfo).includes("undefined" || "null") &&
-      !Object.values(memberInfo).includes("undefined" || "null") &&
-      subscriptions.length > 0
-    ) {
-      const eventSources = subscriptions
-        .map((subscription: Subscription) => {
-          const pushUrl = `${process.env.REACT_APP_DEV_URL}/api/listen?mentorName=${subscription.mentorName}&userName=${memberInfo.nickname}&email=${memberInfo.email}`;
+  if (
+    JSON.stringify(memberInfo) !== "{}" &&
+    !Object.keys(memberInfo).includes("undefined" || "null") &&
+    !Object.values(memberInfo).includes("undefined" || "null")
+  ) {
+    useEffect(() => {
+      if (
+        JSON.stringify(memberInfo) !== "{}" &&
+        !Object.keys(memberInfo).includes("undefined" || "null") &&
+        !Object.values(memberInfo).includes("undefined" || "null") &&
+        subscriptions.length > 0
+      ) {
+        const eventSources = subscriptions
+          .map((subscription: Subscription) => {
+            const pushUrl = `${process.env.REACT_APP_DEV_URL}/api/listen?mentorName=${subscription.mentorName}&userName=${memberInfo.nickname}&email=${memberInfo.email}`;
 
-          const pushEs = new EventSource(pushUrl);
+            const pushEs = new EventSource(pushUrl);
 
-          pushEs.addEventListener("push", (e) => {
-            // 여기서 알림을 생성합니다.
-            toast(e.data);
-            console.log(e.data);
-          });
+            pushEs.addEventListener("push", (e) => {
+              // 여기서 알림을 생성합니다.
+              toast(e.data);
+              console.log(e.data);
+            });
 
-          pushEs.addEventListener("error", (e) => {
-            console.log(e);
-            if (retryCount < 3) {
-              setRetryCount(retryCount + 1);
-              setTimeout(() => {
-                pushEs.close();
-              }, 1000 * 10); // 10초 후에 재연결 시도
-            } else {
-              // 재시도가 3번 실패한 경우, 1시간 후에 재연결 시도
-              setTimeout(() => {
-                pushEs.close();
-                setRetryCount(0);
-              }, 1000 * 60 * 60);
-            }
-          });
+            pushEs.addEventListener("error", (e) => {
+              console.log(e);
+              if (retryCount < 3) {
+                setRetryCount(retryCount + 1);
+                setTimeout(() => {
+                  pushEs.close();
+                }, 1000 * 10); // 10초 후에 재연결 시도
+              } else {
+                // 재시도가 3번 실패한 경우, 1시간 후에 재연결 시도
+                setTimeout(() => {
+                  pushEs.close();
+                  setRetryCount(0);
+                }, 1000 * 60 * 60);
+              }
+            });
 
-          return [pushEs];
-        })
-        .flat();
+            return [pushEs];
+          })
+          .flat();
 
-      // 컴포넌트가 언마운트될 때 이벤트 소싱 요청들을 닫습니다.
-      return () => {
-        eventSources.forEach((es: { close: () => any }) => es.close());
-      };
-    }
-  }, [subscriptions, memberInfo]);
+        // 컴포넌트가 언마운트될 때 이벤트 소싱 요청들을 닫습니다.
+        return () => {
+          eventSources.forEach((es: { close: () => any }) => es.close());
+        };
+      }
+    }, [subscriptions, memberInfo]);
 
-  useEffect(() => {
-    if (
-      JSON.stringify(memberInfo) !== "{}" &&
-      !Object.keys(memberInfo).includes("undefined" || "null") &&
-      !Object.values(memberInfo).includes("undefined" || "null") &&
-      scheduleSubscriptions.length > 0
-    ) {
-      const eventSources = scheduleSubscriptions
-        .map((scheduleSubscriptions: ScheduleSubscriptions) => {
-          const scheduleUrl = `${process.env.REACT_APP_DEV_URL}/api/listen/schedule?mentorName=${scheduleSubscriptions.mentorName}&userName=${memberInfo.nickname}&email=${memberInfo.email}&time=${scheduleSubscriptions.startTime}&roomName=${scheduleSubscriptions.roomName}`;
+    useEffect(() => {
+      if (
+        JSON.stringify(memberInfo) !== "{}" &&
+        !Object.keys(memberInfo).includes("undefined" || "null") &&
+        !Object.values(memberInfo).includes("undefined" || "null") &&
+        scheduleSubscriptions.length > 0
+      ) {
+        const eventSources = scheduleSubscriptions
+          .map((scheduleSubscriptions: ScheduleSubscriptions) => {
+            const scheduleUrl = `${process.env.REACT_APP_DEV_URL}/api/listen/schedule?mentorName=${scheduleSubscriptions.mentorName}&userName=${memberInfo.nickname}&email=${memberInfo.email}&time=${scheduleSubscriptions.startTime}&roomName=${scheduleSubscriptions.roomName}`;
 
-          const scheduleEs = new EventSource(scheduleUrl);
+            const scheduleEs = new EventSource(scheduleUrl);
 
-          scheduleEs.addEventListener("schedule", (e) => {
-            // 여기서 알림을 생성합니다.
-            toast(e.data);
-            console.log(e.data);
-          });
+            scheduleEs.addEventListener("schedule", (e) => {
+              // 여기서 알림을 생성합니다.
+              toast(e.data);
+              console.log(e.data);
+            });
 
-          scheduleEs.addEventListener("error", (e) => {
-            console.log(e);
-            if (retryCount < 3) {
-              setRetryCount(retryCount + 1);
-              setTimeout(() => {
-                scheduleEs.close();
-              }, 1000 * 10); // 10초 후에 재연결 시도
-            } else {
-              // 재시도가 3번 실패한 경우, 1시간 후에 재연결 시도
-              setTimeout(() => {
-                scheduleEs.close();
-                setRetryCount(0);
-              }, 1000 * 60 * 60);
-            }
-          });
+            scheduleEs.addEventListener("error", (e) => {
+              console.log(e);
+              if (retryCount < 3) {
+                setRetryCount(retryCount + 1);
+                setTimeout(() => {
+                  scheduleEs.close();
+                }, 1000 * 10); // 10초 후에 재연결 시도
+              } else {
+                // 재시도가 3번 실패한 경우, 1시간 후에 재연결 시도
+                setTimeout(() => {
+                  scheduleEs.close();
+                  setRetryCount(0);
+                }, 1000 * 60 * 60);
+              }
+            });
 
-          return [scheduleEs];
-        })
-        .flat();
+            return [scheduleEs];
+          })
+          .flat();
 
-      // 컴포넌트가 언마운트될 때 이벤트 소싱 요청들을 닫습니다.
-      return () => {
-        eventSources.forEach((es: { close: () => any }) => es.close());
-      };
-    }
-  }, [scheduleSubscriptions, memberInfo]);
+        // 컴포넌트가 언마운트될 때 이벤트 소싱 요청들을 닫습니다.
+        return () => {
+          eventSources.forEach((es: { close: () => any }) => es.close());
+        };
+      }
+    }, [scheduleSubscriptions, memberInfo]);
+  }
 
   // 모든 subscriptions와 scheduleSubscriptions 삭제
   const resetSubscriptions = () => {

abc545ab095eb3d857fd0efebf0a8876aed2835f
EagerProgrammer <yth960617@unist.ac.kr>
2023-04-25 18:02:42 +0900

    Fix : Pathname 기능 재수정

--- src/components/problemRegister/ProblemRegisterBox.tsx
+++ src/components/problemRegister/ProblemRegisterBox.tsx
@@ -100,7 +100,7 @@ const ProblemRegisterBox = () => {
   const handleCheckboxChange = (value: boolean) => {
     setIsObjective(!value);
   };
-  const DeletedFileClick = (setS3File: any) => {
+  const DeletedFileClick = () => {
     setS3File('');
   }
 
@@ -307,7 +307,7 @@ const ProblemRegisterBox = () => {
           </button>)}
           {isimageOpen && (
           < button className="py-2 px-4 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-blue-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 mt-3"
-          onClick={() => DeletedFileClick(setS3File)}
+          onClick={() => DeletedFileClick()}
           ></button>)}
       </div>
       {isimageOpen && location?.state?.pathname && (

930db4baadcc05fc63d29f63d9208cba819e1d4a
moon <xmun74@naver.com>
2023-04-25 12:30:13 +0900

    feat: MyPage - 멘토등록, 닉네임 공백제거, 회원탈퇴, Tag 수정

--- src/components/mypage/NicknameInput.tsx
+++ src/components/mypage/NicknameInput.tsx
@@ -10,8 +10,28 @@ const NicknameInput = ({
   prevNickname: string;
 }) => {
   const [nickname, setNickname] = useState("");
-  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
+  const [errMsg, setErrMsg] = useState("");
+  const handleNicknameChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    let value = target.value;
+
+    // 공백만 입력 방지 (2~10자 이하/공백 제거)
+    if (value.trim() !== "") {
+      if (value.length < 2) {
+        return setErrMsg("2자 이상의 닉네임을 입력해주세요");
+      }
+      if (value.length > 10) {
+        return setErrMsg("10자 이하의 닉네임을 입력해주세요");
+      }
+      if (/\s/.exec(value)) {
+        value = value.replace(/\s|  /gi, ""); // 빈값, 공백 제거
+        setNickname(value);
+        return setErrMsg("공백을 입력했으므로 공백을 제거합니다");
+      } else {
+        setErrMsg("");
+        setNickname(value);
+      }
+    }
   };
   return (
     <>
@@ -22,8 +42,11 @@ const NicknameInput = ({
         placeholder="닉네임"
         className="sign_input mb-2 w-full"
         defaultValue={`${prevNickname}`}
-        onChange={handleNicknameChange}
+        onKeyUp={handleNicknameChange}
+        minLength={2}
+        maxLength={10}
       />
+      <div className="text-xs text-red-500">{errMsg && errMsg}</div>
       <div className="flex justify-end mt-10">
         <ConfirmBtn
           type="submit"

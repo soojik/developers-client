2d468c53994de93976382ddc5bf085858f946d35
moon <xmun74@naver.com>
2023-04-26 00:41:31 +0900

    style: carousel banner

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import LeftArrowIcon from "./icons/LeftArrowIcon";
 import RightArrowIcon from "./icons/RightArrowIcon";
+import BannerOne from "../images/Banner1.png";
+import BannerTwo from "../images/Banner2.png";
+import BannerThree from "../images/Banner3.png";
 
 const Carousel = () => {
   const carouselImages = useRef([
     {
-      src: `https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80`,
+      src: BannerOne,
     },
     {
-      src: `https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80`,
+      src: BannerTwo,
     },
     {
-      src: `https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80`,
+      src: BannerThree,
     },
   ]);
 

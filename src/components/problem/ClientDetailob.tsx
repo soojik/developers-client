493d351b0ce1c4f033423ec3a922fb660e72f946
moon <xmun74@naver.com>
2023-04-28 15:25:24 +0900

    style: ob, sb detail

--- src/components/problem/ClientDetailob.tsx
+++ src/components/problem/ClientDetailob.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
 import ProblemPointModal from "components/problem/ProblemPointModal";
 import LevelIcon from "components/icons/LevelIcon";
 import ViewIcon from "components/icons/ViewIcon";
@@ -8,9 +8,8 @@ import LikesIcon from "components/icons/LikesIcon";
 import Tags from "components/Tags";
 import { useRecoilValue } from "recoil";
 import { memberInfoState } from "recoil/userState";
-import ProblemSolved from "pages/ProblemSolved";
 import { axiosInstance } from "apis/axiosConfig";
-
+import CheckIcon from "components/icons/CheckIcon";
 
 interface ProblemBoxProps {
   problemId: number;
@@ -29,7 +28,6 @@ interface ProblemBoxProps {
   pathname: string;
 }
 const ProblemDetail = () => {
-  const [isimageOpen, setIsImageOpen] = useState(false);
   const location = useLocation();
   const { problemId, member } = useParams();
   const [detail, setDetail] = useState<ProblemBoxProps>();
@@ -40,7 +38,10 @@ const ProblemDetail = () => {
   const [pointAdd, setPointAdd] = useState(0); // 포인트 -> 연산값이 실제로 연산된 후 보내져야한다.
   const [isEditing, setIsEditing] = useState(true); // 수정 모드인지 여부를 true로 변경
   const [radioDisabled, setRadioDisabled] = useState(false);
-  const [selectedNumberState, setSelectedNumberState] = useState<number | null>(null);
+  const [radioCheckedIdx, setRadioCheckedIdx] = useState(0);
+  const [selectedNumberState, setSelectedNumberState] = useState<number | null>(
+    null
+  );
   const [problemSolved, setProblemSolved] = useState(false); // 문제 풀이 유무 DB저장을 위해서 만듦
   const [answerWriter, setAnswerWriter] = useState(""); //문제 작성자를 받아오기 위한값===
   const [likes, setLikes] = useState(detail?.likes || 0);
@@ -48,7 +49,6 @@ const ProblemDetail = () => {
   const { memberInfo, memberId, isLoggedIn } = useRecoilValue(memberInfoState);
   const [solved, setSolved] = useState(detail?.solved);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
 
   const sessionAnswer = sessionStorage.getItem("answer") || ""; // 값 null처리
 
@@ -62,18 +62,13 @@ const ProblemDetail = () => {
   };
 
   const handleModalClose = () => {
-    if(modalTitle === "정답입니다!"){
+    if (modalTitle === "정답입니다!") {
       setProblemSolved(true);
       setSolved(true);
     }
-    
+
     setIsModalOpen(false);
   };
-
-  useEffect(() => {
-    console.log(problemSolved);
-  }, [problemSolved]);
-
 
   useEffect(() => {
     const fetchProblemDetail = async () => {
@@ -86,6 +81,9 @@ const ProblemDetail = () => {
         setLikes(response.data?.data?.likes);
         setAnswer(response.data.data.answer);
         setProblemSolved(response.data.data.solved);
+        if (response.data.data.answer && response.data.data.solved) {
+          setRadioCheckedIdx(Number(response.data.data.answer));
+        }
         setSolved(response.data?.data?.solved);
         const answerCandidateArray = answerCandidateString;
         setAnswerCandidate(answerCandidateArray);
@@ -108,17 +106,13 @@ const ProblemDetail = () => {
 
   const handleRadioClick = (selectedNumber: number) => {
     setSelectedNumberState(selectedNumber);
-    console.log("Selected radio button:", selectedNumber);
-    console.log("Correct answer:", detail.answer);
+    // console.log("Selected Num:", selectedNumber, "Answer Num:", detail.answer);
     // 여기에서 번호를 전송하거나 다른 작업을 수행할 수 있습니다.
     const answerNumber = selectedNumber.toString(); // 문자열을 숫자로 변환
     if (answerNumber === detail.answer) {
-      console.log("정답처리"); // 추후 삭제 예정
-      console.log(detail.answer);
       setModalTitle("정답입니다!");
       setPointAdd(10);
     } else {
-      console.log("오답처리");
       setModalTitle("오답입니다");
       setPointAdd(0);
     }
@@ -140,7 +134,6 @@ const ProblemDetail = () => {
     }
     setIsEditing(false);
     setRadioDisabled(true);
-
   };
 
   const handleLikeButtonClick = async () => {
@@ -178,135 +171,131 @@ const ProblemDetail = () => {
     }
   };
 
-
-
   return (
     <>
-      <div>
-        <p
-          style={{
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-            marginBottom: "10px",
-          }}
-        >
-          문제 번호:{" "}
-          <span style={{ color: "#007BFF", fontSize: "1.5rem" }}>
-            {detail.problemId}
-          </span>
-        </p>
+      <div className="font-bold text-xl mb-4">
+        {detail.problemId}. {detail.title}
       </div>
-      <div className="flex justify-between items-center mb-3 ">
-        <div className="flex justify-center items-center">
-          {detail.level === "gold" ? (
-            <LevelIcon fill="#D9B600" width={30} height={30} />
-          ) : detail.level === "silver" ? (
-            <LevelIcon fill="gray" width={30} height={30} />
-          ) : (
-            <LevelIcon fill="#AD5600" width={30} height={30} />
-          )}
-        </div>
-      </div>
-      <div className="flex justify-between items-center mb-3">
-        <div className="flex justify-center items-center text-m text-bold">
-          {detail.type === "answer" ? "주관식" : "객관식"}
-          <div className="flex text-xm ml-4 items-center text-black">
-            <ViewIcon fill="black" width={18} height={18} />
-            &nbsp;{detail.views}
-          </div>
-        </div>
-        {isLoggedIn === false ? (
-          <div></div>
-        ) : (
-          <p style={{ fontSize: "1.2rem", marginBottom: "10px" }}>
-            문제 해결 여부:{" "}
-            <span
-              className={solved ? "solved" : "unsolved"}
-              style={{ fontWeight: "bold" }}
-            >
-              {solved ? "해결" : "미해결"}
+      <div className="flex justify-between items-center mb-5 pb-5 border-b">
+        <div className="flex flex-col text-slate-400 text-xs sm:text-sm">
+          <div className="flex mb-4 items-center">
+            <div className="mr-4">
+              {detail.level === "gold" ? (
+                <LevelIcon fill="#D9B600" width={18} height={18} />
+              ) : detail.level === "silver" ? (
+                <LevelIcon fill="gray" width={18} height={18} />
+              ) : (
+                <LevelIcon fill="#AD5600" width={18} height={18} />
+              )}
+            </div>
+            <span className="mr-4">
+              {detail.type === "answer" ? "주관식" : "객관식"}
             </span>
-          </p>
+            <span className="mr-4">{detail.writer}</span>
+            <div className="flex items-center mr-4">
+              <ViewIcon fill="rgb(148 163 184)" width={20} height={20} />
+              &nbsp;{detail.views}
+            </div>
+          </div>
+          <Tags tagList={detail?.hashTag ? detail?.hashTag?.split(",") : []} />
+        </div>
+        {isLoggedIn === false ? null : (
+          <span
+            className={`${solved ? "solved" : "unsolved"} font-bold text-sm`}
+          >
+            {solved ? (
+              <div className="flex items-center py-1 p-2 border rounded-lg border-green-600 text-green-600">
+                <CheckIcon stroke="rgb(22 163 74)" width={18} height={18} />
+                &nbsp;해결
+              </div>
+            ) : (
+              <div className="flex items-center py-1 p-2 border rounded-lg border-slate-400 text-slate-400">
+                <CheckIcon stroke="lightGray" width={18} height={18} />
+                &nbsp;미해결
+              </div>
+            )}
+          </span>
         )}
       </div>
-      <div className=" bg-gray-200 px-20 py-10 rounded">
-        <div className="mt-4">
-          <div className="flex-container">
-            <h2 className="text-lg font-medium leading-6 text-gray-900 bg-white rounded">
-              제목: {detail.title}
-            </h2>
-          </div>
-          <p className="bg-white rounded mt-4 ">작성자: {detail.writer}</p>
-          <div className="mt-4">
-            <p className="text-sm text-gray-500 bg-white rounded py-9">
-              {detail.content}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        {!detail.pathname ? (
-          <div></div>
-        ) : (
+      <div className="flex justify-between pb-20">
+        <div className="text-sm text-gray-500">{detail.content}</div>
+        {detail && (
           <button
-            className="py-1 px-2 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-blue-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 mt-5"
-            onClick={() => {
-              setIsImageOpen(!isimageOpen);
-            }}
+            className="flex  items-center ml-3 cursor-pointer w-[70px]"
+            disabled={isLiked}
+            onClick={handleLikeButtonClick}
           >
-            이미지 미리보기
+            <LikesIcon
+              fill={`${isLiked ? "rgb(37 99 235)" : "rgb(148 163 184)"}`}
+              width={25}
+              height={25}
+            />
+            &nbsp; {likes}
           </button>
         )}
       </div>
-      {isimageOpen && (
-        <p className="flex">
-          <img src={detail.pathname} alt="이미지를 불러오지 못했습니다." />
-        </p>
+      {!detail.pathname ? (
+        <div></div>
+      ) : (
+        <img
+          src={detail.pathname}
+          alt="problem-img"
+          className="max-w-[340px] mb-10"
+        />
       )}
+
       {isLoggedIn === false ? (
         <div></div>
       ) : (
-        <div className="mt-4">
-          <label
-            htmlFor="answer"
-            className="block text-sm font-medium text-gray-700"
-          >
-            보기
-          </label>
-          <div className="flex flex-col items-start space-y-2">
-            {detail.answerCandidate.map((candidate: string, index: number) => (
-              <div key={index} className="flex items-center">
-                <input
-                  type="radio"
-                  id={`option${index + 1}`}
-                  name="answerCandidate"
-                  value={index + 1}
-                  className="mr-2"
-                  onChange={() => handleRadioClick(index + 1)}
-                  disabled={radioDisabled}
-                />
-                <label htmlFor={`option${index + 1}`} className="text-sm">
-                  {candidate}
-                </label>
-              </div>
-            ))}
-          </div>
-        </div>
+        <ul className="flex flex-col space-y-2">
+          {detail.answerCandidate.map((candidate: string, index: number) => (
+            <li key={candidate}>
+              <input
+                type="radio"
+                id={`option${index + 1}`}
+                name="answerCandidate"
+                value={index + 1}
+                className={`mr-2 w-5 h-5 hidden`}
+                disabled={radioDisabled}
+              />
+              <label
+                htmlFor={`option${index + 1}`}
+                className={`flex items-center text-sm cursor-pointer max-w-[420px] rounded-lg p-3 hover:bg-opacity-80 ${
+                  radioCheckedIdx === index + 1
+                    ? "bg-blue-500 text-white"
+                    : "bg-blue-100 bg-opacity-50"
+                }
+                ${
+                  solved || radioDisabled
+                    ? "pointer-events-none text-slate-400 bg-opacity-60"
+                    : null
+                }`}
+                onClick={() => {
+                  setRadioCheckedIdx(index + 1);
+                  handleRadioClick(index + 1);
+                }}
+              >
+                <div
+                  className={`flex items-center justify-center w-5 h-5 text-xs border border-slate-600 rounded-full ${
+                    radioCheckedIdx === index + 1
+                      ? "border-0 bg-white text-blue-500"
+                      : null
+                  }
+                  ${
+                    solved || radioDisabled
+                      ? "border-slate-400 text-slate-400"
+                      : null
+                  }`}
+                >
+                  {index + 1}
+                </div>
+                <span>&nbsp; {candidate}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
       )}
-      {detail && (
-        <button
-          className=" flex py-2 px-4 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-blue-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0 mt-2"
-          onClick={handleLikeButtonClick}
-          disabled={isLiked}
-        >
-          <LikesIcon fill="black" width={14} height={14}></LikesIcon>
-          {likes}
-        </button>
-      )}
-      <div className="flex">
-        <p className="text-bold text-blue-500"> 해시태그:&nbsp;</p>{" "}
-        <Tags tagList={detail?.hashTag ? detail?.hashTag?.split(",") : []} />
-      </div>
+
       {isLoggedIn === false ? (
         <div></div>
       ) : (
@@ -349,7 +338,7 @@ const ProblemDetail = () => {
                           point={pointAdd}
                           isOpen={modalOpen}
                           onClose={handleModalClose} // onClose 함수 전달
-                          />
+                        />
                       )}
                     </div>
                   )}
@@ -362,6 +351,4 @@ const ProblemDetail = () => {
     </>
   );
 };
-export default ProblemDetail; 
-
-
+export default ProblemDetail;
